0639e243afff470ce51d9313430e19c090b855ff
J_D_L <jocelyndeleon52@gmail.com>
2021-05-15 18:24:38 -0700

    add hercules

--- Analysis.sql
+++ Analysis.sql
@@ -24,7 +24,8 @@ ORDER BY emp_no;
 
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+SELECT * FROM employees
+WHERE first_name = 'Hercules' AND last_name like 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 

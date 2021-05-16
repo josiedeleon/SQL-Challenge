c29b7d222ac54be6bccff63e965e4bf4c17da064
J_D_L <jocelyndeleon52@gmail.com>
2021-05-15 19:23:50 -0700

    add freq count

--- Analysis.sql
+++ Analysis.sql
@@ -78,4 +78,9 @@ WHERE departments.dept_name in ('Sales', 'Development')
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
+
 

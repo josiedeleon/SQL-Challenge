078641d121bc3c3734bf0e6b17b00fad5d9e0ebc
J_D_L <jocelyndeleon52@gmail.com>
2021-05-15 18:43:32 -0700

    employee department

--- Analysis.sql
+++ Analysis.sql
@@ -21,7 +21,16 @@ ORDER BY emp_no;
 
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT
+employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees 
+INNER JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no=dept_emp.dept_no
+order by emp_no;
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT * FROM employees
